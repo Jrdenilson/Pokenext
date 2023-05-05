b76475c4e79db952ef9f131d5279e533b450eb04
Denilson Junior <jrdenilson1@outlook.com>
2023-05-05 13:29:12 -0300

    Enviando pokenext app

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from "/styles/Footer.module.css"
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <p><span>PokeNext</span> &copy; 2023</p>
+        </footer>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+import styles from "../styles/About.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1>Sobre o projeto</h1>
+      <p>Charizard</p>
+      <Image
+        src="/images/charizard.png"
+        width="300"
+        height="300"
+        alt="Charizard"
+      />
+    </div>
+  );
+}
